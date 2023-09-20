081e17f6b6291ad62a853749148d19e0c6a60be5
Lucas Couto <90484947+lucasadriano97@users.noreply.github.com>
2023-09-20 15:20:00 -0300

    Created Formação vals to insert in the program

--- desafio.kt
+++ desafio.kt
@@ -18,13 +18,24 @@ data class Formacao(val nome: String, val nivel: Nivel, val habilidades: List<Ad
 
 
 fun main() {
+
     // Criação de listas de habilidades
     val habilidadesJedi = listOf(            
         AdquirirHabilidade("Força", 10),
         AdquirirHabilidade("Sabre de Luz", 5),
         AdquirirHabilidade("Telecinese", 8)
     )
+    val habilidadesPadawan = listOf(
+        AdquirirHabilidade("Força Básica", 5),
+        AdquirirHabilidade("Treinamento com Sabre", 3),
+        AdquirirHabilidade("Meditar", 4)
+    )
+    val habilidadesNPC = listOf(
+        AdquirirHabilidade("Tarefas Domésticas", 1)
+    )
     
-    
-    
+    // Criação de formações com diferentes níveis e habilidades
+    val formacaoJedi = Formacao("Formação Jedi", Nivel.Jedi, habilidadesJedi)  
+    val formacaoPadawan = Formacao("Formação Padawan", Nivel.Padawan, habilidadesPadawan)
+    val formacaoNPC = Formacao("Formação NPC", Nivel.NPC, habilidadesNPC)
     }
